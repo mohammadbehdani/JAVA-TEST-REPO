abb4c7d901a73212561e21a0888e233cb75f89c8
Oskar Ray-Frayssinet <oskar.rf@live.se>
2024-05-15 16:51:22 +0200

    Update Excercise-1-1-Test.java

--- JU24/Javaprogrammering60yhp/Excercise-1-1-Test.java
+++ JU24/Javaprogrammering60yhp/Excercise-1-1-Test.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-public class MainTest {
+public class Excercise-1-1-Test {
     private final PrintStream originalOut = System.out;
     private ByteArrayOutputStream outContent;
 
